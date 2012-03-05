f26125ec9b0aa1e766e5f0086853ac1e7d5edab7
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-03-05 18:14:56 -0500

    trying download script

---
+++ web/download_linux.php
@@ -0,0 +1,9 @@
+<?php
+
+$fileCount = '/r/bcb/genecentric/downloads-linux'
+$downloads = file_get_contents($fileCount)
+file_put_contents($fileCount, $downloads + 1)
+
+header('Location: files/genecentric-1.0.0.tar.gz')
+
+?>
