14aff438c72dd3e9d411ae080168d352f5a69a3b
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-03-05 18:40:04 -0500

    header/footer includes ftw

---
+++ web/includes/footer.php
@@ -0,0 +1,15 @@
+
+  </div>
+  <footer>
+    <p>If you use Genecentric, please cite both:</p>
+    <p>M. Leiserson, D. Tatar, L. Cowen and B. Hescott, "Inferring Mechanisms of Compensation from E-MAP and SGA Data Using Local Search Algorithms for Max Cut," Recomb 2011, pp. 154-167.</p>
+    <p><strong>and</strong></p>
+    <p>Gallant, Leiserson, Cowen and Hescott, "Genecentric: A package to
+       uncover graph-theoretic structure in high-throughput epistasis data."
+       Submitted for publication.</p>
+  </footer>
+
+
+</body>
+</html>
+
--- web/index.php
+++ web/index.php
@@ -1,33 +1,5 @@
-<!doctype html>
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title></title>
-  <meta name="description" content="">
+<?php require_once('includes/header.php'); ?>
 
-  <meta name="viewport" content="width=device-width">
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/print.css">
+<h3>Home page</h3>
 
-  <!-- <script src="js/libs/modernizr-2.5.3.min.js"></script> -->
-</head>
-<body>
-  <header>
-
-  </header>
-  <div role="main">
-
-  </div>
-  <footer>
-
-  </footer>
-
-
-</body>
-</html>
-
+<?php require_once('includes/footer.php'); ?>
