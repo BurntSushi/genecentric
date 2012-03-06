9562d21d652f3da9b296891c793b3ada63d6dd41
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-03-05 19:38:30 -0500

    gpl info

--- web/includes/footer.php
+++ web/includes/footer.php
@@ -1,6 +1,8 @@
 
   </div>
   <footer>
+    <p>All source code is released under the <a href="LICENSE">GNU Public
+       License version 2.0</a>.</p>
     <p>If you use Genecentric, please cite both:</p>
     <p>M. Leiserson, D. Tatar, L. Cowen and B. Hescott, "Inferring Mechanisms of Compensation from E-MAP and SGA Data Using Local Search Algorithms for Max Cut," Recomb 2011, pp. 154-167.</p>
     <p><strong>and</strong></p>
