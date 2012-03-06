9dfe2e6172924dd7726d67f747cef6cfb421b933
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-03-05 21:52:46 -0500

    char encoding business

--- web/includes/header.php
+++ web/includes/header.php
@@ -8,7 +8,7 @@ $currentFile = "genecentric-1.0.0";
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-  <meta charset="utf-8">
+  <meta charset="iso-8859-1">
   <title></title>
   <meta name="description" content="">
 
