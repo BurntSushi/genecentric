99de72a14b40b1e5c54bf7cb5089156851a5ce63
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-03-22 17:26:29 -0400

    wow, i forgot to fill in the title tag

--- web/includes/header.php
+++ web/includes/header.php
@@ -9,15 +9,19 @@ $currentFile = "genecentric-1.0.0";
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
   <meta charset="iso-8859-1">
-  <title></title>
-  <meta name="description" content="">
-
+  <title>Genecentric - A package to uncover graph-theoretic structure in 
+         high-throughput epistasis data.</title>
+  <meta name="description" content="Genecentric is a package that performs two 
+  main tasks: Implement the between-pathway module (BPM) generation algorithm 
+  with pruning as described in Leiserson et al, 2010. In particular, the 
+  implementation is automatically parallelized when more than one CPU is 
+  present.  Perform GO enrichment on generated BPMs using the JSON-RPC API of 
+  FuncAssociate 2.0.">
   <meta name="viewport" content="width=device-width">
+
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/print.css">
-
-  <!-- <script src="js/libs/modernizr-2.5.3.min.js"></script> -->
 </head>
 <body>
   <div id="container"><div id="container2">
