67b290d22756960c0a88395fb78cbeb12ef96e05
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-04-16 19:08:14 -0400

    predict based on the bpm sides that the test edges are in

--- bpm/cmdargs/predict.py
+++ bpm/cmdargs/predict.py
@@ -31,6 +31,10 @@ aa('-e', '--essential-list', dest='essentials', type=str, default=None,
    help='The location of an essential gene list file. (One gene per line.) '
         'Any genes in this file will be excluded from the set of genes used '
         'to generate BPMs.')
+aa('-c', '--gene-ratio', dest='C', type=float, default=0.90,
+   metavar='RATIO', help='Gene ratio threshold')
+aa('-j', '--jaccard', dest='jaccard', type=float, default=0.66,
+   metavar='JACCARD_INDEX', help='Jaccard Index threshold')
 aa('-m', '--num-bipartitions', dest='M', type=int, default=250,
    metavar='NUMBER_BIPARTITIONS', help='Number of bipartitions to generate')
 aa('-r', '--opposite-ratio', dest='R', type=float, default=0.65,
@@ -40,10 +44,22 @@ aa('-t', '--test-ratio', dest='T', type=float, default=0.05,
 aa('--no-squaring', dest='squaring', action='store_false',
    help='If set, genetic interaction scores will not be squared. '
         'Squaring typically speeds convergence.')
+aa('--minimum-size', dest='min_size', type=int, default=3,
+   metavar='MIN_SIZE', 
+   help='Minimum size of BPM. Smaller BPMs are pruned. '
+        'Set to 0 to disable.')
+aa('--maximum-size', dest='max_size', type=int, default=25,
+   metavar='MAX_SIZE', 
+   help='Maximum size of BPM. Bigger BPMs are pruned. '
+        'Set to 0 to disable.')
 aa('-p', '--processes', dest='processes', type=int, default=__cpus,
    metavar='PROCESSES',
    help='The number of processes to run concurrently. If set to '
         '1, the multiprocessing module will not be used.')
+aa('--no-jaccard', dest='pruning', action='store_false',
+   help='If set, no pruning will occur. Note that --minimum-size and '
+        '--maximum-size will still have an effect. Set those to 0 to '
+        'disable that pruning.')
 aa('--no-progress', dest='progress', action='store_false',
    help='If set, the progress bar will not be shown.')
 aa('-v', '--verbose', dest='verbose', action='store_true',
--- bpm/predict.py
+++ bpm/predict.py
@@ -5,13 +5,31 @@ on sets of partitions.
 
 from bpm import conf, geneinter
 
+def bpm_prediction(bpms):
+    predictions = [] # [(gene1, gene2, # opp, # same, # nil, real score)]
+    for g1, g2 in geneinter.test:
+        same = opp = nopred = 0
+        for A, B in bpms:
+            if (g1 in A and g2 in A) or (g1 in B and g2 in B):
+                same += 1
+            elif (g1 in A and g2 in B) or (g1 in B and g2 in A):
+                opp += 1
+            else:
+                nopred += 1
+        score = geneinter.test[(g1, g2)]
+        predictions.append((g1, g2, opp, same, nopred, score))
+
+    return sorted(predictions,
+                  key=lambda (g1, g2, o, s, n, r): (o > s, o, s, n),
+                  reverse=True)
+
 def negative_edges(parts):
     '''
     Traverse all 'test' edges, checks whether the endpoints are in opposing
     partitions R% of the time, and only reports those edges with a confidence
     score.
     '''
-    predictions = [] # [(gene1, gene2, confidence, real weight)]
+    predictions = [] # [(gene1, gene2, confidence, real score)]
     for g1, g2 in geneinter.test:
         opprat = ratio_opposite(parts, g1, g2)
         if opprat >= conf.R:
